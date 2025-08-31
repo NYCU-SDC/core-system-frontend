3b870659e481d697feec2d0e80328e6f731cc570
Kyle9410-Chen <kyle941010@gmail.com>
2025-08-31 15:14:27 +0800

    style: improve formatting in Home component and update font import syntax

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,18 @@ const Home = () => {
 	return (
 		<div className="h-screen text-center flex flex-col items-center justify-center font-[Quantico]">
 			<p className="text-5xl text-slate-700 tracking-widest font-bold">Core System</p>
-			<p className="mt-[22px] text-sm text-slate-300 tracking-[6px]">/* Everything all at once */</p>
-			<Button className="mt-[41px] font-bold"
-					variant="outline"
-					onClick={() => {toast("Hello World!")}}>Login with Google</Button>
-
+			<p className="mt-[22px] text-sm text-slate-300 tracking-[6px]">
+				/* Everything all at once */
+			</p>
+			<Button
+				className="mt-[41px] font-bold"
+				variant="outline"
+				onClick={() => {
+					toast("Hello World!");
+				}}
+			>
+				Login with Google
+			</Button>
 		</div>
 	);
 };
