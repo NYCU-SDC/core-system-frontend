26a43c08cb8c690cdfc55a09d812fcb2033c7dbd
Kyle9410-Chen <kyle941010@gmail.com>
2025-09-10 14:28:11 +0800

    feat: implement Google login functionality and update navigation logic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,21 @@
-import { toast } from "sonner";
 import { Button } from "@/components/ui";
 
 const Home = () => {
+
+    const login = () => {
+        window.location.href = `${import.meta.env.VITE_API_BASE_URL}/auth/login/oauth/google?r=${window.location.origin}/auth/callback`;
+    }
+
 	return (
 		<div className="h-screen text-center flex flex-col items-center justify-center font-[Quantico]">
 			<p className="text-5xl text-slate-700 tracking-widest font-bold">Core System</p>
-			<p className="mt-[22px] text-sm text-slate-300 tracking-[6px]">
+			<p className="m-22 text-sm text-slate-300 tracking-[6px]">
 				/* Everything all at once */
 			</p>
 			<Button
 				className="mt-[41px] font-bold"
 				variant="outline"
-				onClick={() => {
-					toast("Hello World!");
-				}}
+				onClick={login}
 			>
 				Login with Google
 			</Button>
