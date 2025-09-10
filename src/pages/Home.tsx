b1b9dad073ee25858474421459fdfe3d8e98e1a1
Kyle9410-Chen <kyle941010@gmail.com>
2025-09-10 14:46:48 +0800

    fix: adjust margin syntax in Home component for consistency

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
 	return (
 		<div className="h-screen text-center flex flex-col items-center justify-center font-[Quantico]">
 			<p className="text-5xl text-slate-700 tracking-widest font-bold">Core System</p>
-			<p className="m-22 text-sm text-slate-300 tracking-[6px]">
+			<p className="mt-[22px] text-sm text-slate-300 tracking-[6px]">
 				/* Everything all at once */
 			</p>
 			<Button className="mt-[41px] font-bold" variant="outline" onClick={login}>
