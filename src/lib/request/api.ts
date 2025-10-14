05f579a2b67b95accf8d21cdcd10c9cf8ac80fae
mochizuki <1p41p4jejo@gmail.com>
2025-10-14 21:12:01 +0800

    style: fix coding style

--- src/lib/request/api.ts
+++ src/lib/request/api.ts
@@ -1,14 +1,13 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function api<T>(path: string, options: RequestInit = {}): Promise<T> {
-	if (!options.headers["Content-Type"])
-		options.headers["Content-Type"] = "application/json";
+	if (!options.headers["Content-Type"]) options.headers["Content-Type"] = "application/json";
 
 	const res = await fetch(`${BASE_URL}${path}`, {
 		...options,
 		headers: {
 			"Content-Type": "application/json",
-			...options.headers,
+			...options.headers
 		}
 	});
 
