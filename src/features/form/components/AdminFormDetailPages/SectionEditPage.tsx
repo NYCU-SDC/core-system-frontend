270b8105a0dfb313462ace1004a63c7dfa1c4e25
Kyle9410-Chen <kyle941010@gmail.com>
2026-01-19 21:07:02 +0800

    feat: add section question edit page

---
+++ src/features/form/components/AdminFormDetailPages/SectionEditPage.tsx
@@ -0,0 +1,105 @@
+import { Button, Input } from "@/shared/components";
+import { Calendar, CaseSensitive, CloudUpload, Ellipsis, LayoutList, List, ListOrdered, Rows3, SquareCheckBig, TextAlignStart } from "lucide-react";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import styles from "./SectionEditPage.module.css";
+import { QuestionCard } from "./components/QuestionCard";
+
+type Question = {
+	type: "SHORT_TEXT" | "LONG_TEXT" | "SINGLE_CHOICE" | "MULTIPLE_CHOICE" | "DROPDOWN" | "DETAILED_MULTIPLE_CHOICE" | "DATE" | "UPLOAD_FILE" | "LINEAR_SCALE" | "RANKING";
+	title: string;
+	description: string;
+	onTitleChange?: (newTitle: string) => void;
+	onDescriptionChange?: (newDescription: string) => void;
+};
+
+type NewQuestion = {
+	icon: React.ReactNode;
+	text: string;
+	type: Question["type"];
+};
+
+export const AdminSectionEditPage = () => {
+	const { formid } = useParams();
+	const navigate = useNavigate();
+
+	const [questions, setQuestions] = useState<Question[]>([]);
+
+	const newQuestionOptions: NewQuestion[] = [
+		{ icon: <CaseSensitive />, text: "文字簡答", type: "SHORT_TEXT" },
+		{ icon: <TextAlignStart />, text: "文字詳答", type: "LONG_TEXT" },
+		{ icon: <List />, text: "單選選擇題", type: "SINGLE_CHOICE" },
+		{ icon: <SquareCheckBig />, text: "核取方塊", type: "MULTIPLE_CHOICE" },
+		{ icon: <Rows3 />, text: "下拉選單", type: "DROPDOWN" },
+		{ icon: <LayoutList />, text: "詳細核取方塊", type: "DETAILED_MULTIPLE_CHOICE" },
+		{ icon: <CloudUpload />, text: "檔案上傳", type: "UPLOAD_FILE" },
+		{ icon: <Ellipsis />, text: "線性刻度", type: "LINEAR_SCALE" },
+		{ icon: <ListOrdered />, text: "排序", type: "RANKING" },
+		{ icon: <Calendar />, text: "日期選擇", type: "DATE" }
+	];
+
+	const handleBack = () => {
+		navigate(`/orgs/sdc/forms/${formid}/edit`);
+	};
+
+	const handleAddQuestion = (type: Question["type"]) => {
+		setQuestions([...questions, { type, title: "", description: "" }]);
+	};
+
+	const handleRemoveQuestion = (index: number) => {
+		const updatedQuestions = [...questions];
+		console.log("Removing question at index:", index);
+		updatedQuestions.splice(index, 1);
+		setQuestions(updatedQuestions);
+	};
+
+	const handleTitleChange = (index: number, newTitle: string) => {
+		const updatedQuestions = [...questions];
+		updatedQuestions[index].title = newTitle;
+		setQuestions(updatedQuestions);
+	};
+
+	const handleDescriptionChange = (index: number, newDescription: string) => {
+		const updatedQuestions = [...questions];
+		updatedQuestions[index].description = newDescription;
+		setQuestions(updatedQuestions);
+	};
+
+	return (
+		<>
+			<div className={styles.layout}>
+				<div className={styles.content}>
+					<Button onClick={handleBack}>Back</Button>
+					<div className={styles.container}>
+						<section className={styles.card}>
+							<Input placeholder="Section 標題" variant="flushed" themeColor="--comment" textSize="h2" />
+							<Input placeholder="這裡可以寫一段描述" variant="flushed" themeColor="--comment" />
+						</section>
+						{questions.map((question, index) => (
+							<QuestionCard
+								key={index}
+								type={question.type}
+								title={question.title}
+								description={question.description}
+								removeQuestion={() => handleRemoveQuestion(index)}
+								onTitleChange={newTitle => handleTitleChange(index, newTitle)}
+								onDescriptionChange={newDescription => handleDescriptionChange(index, newDescription)}
+							/>
+						))}
+					</div>
+				</div>
+				<div>
+					<div className={styles.sidebar}>
+						<p>新增</p>
+						{newQuestionOptions.map((option, index) => (
+							<button key={index} className={styles.newQuestion} onClick={() => handleAddQuestion(option.type)}>
+								{option.icon}
+								{option.text}
+							</button>
+						))}
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
---
+++ src/features/form/components/AdminFormDetailPages/components/QuestionCard.tsx
@@ -0,0 +1,127 @@
+import { Button, Switch } from "@/shared/components";
+import { Input } from "@/shared/components/Input/Input";
+import { Calendar, CaseSensitive, CloudUpload, Copy, Ellipsis, LayoutList, List, ListOrdered, Rows3, SquareCheckBig, TextAlignStart, Trash2 } from "lucide-react";
+import { useState } from "react";
+import styles from "./QuestionCard.module.css";
+
+export interface QuestionCardProps {
+	type: "SHORT_TEXT" | "LONG_TEXT" | "SINGLE_CHOICE" | "MULTIPLE_CHOICE" | "DROPDOWN" | "DETAILED_MULTIPLE_CHOICE" | "DATE" | "UPLOAD_FILE" | "LINEAR_SCALE" | "RANKING";
+	title?: string;
+	description?: string;
+	onTitleChange?: (newTitle: string) => void;
+	onDescriptionChange?: (newDescription: string) => void;
+	removeQuestion?: () => void;
+}
+
+type typeInfo = {
+	icon: React.ReactNode;
+	label: string;
+};
+
+const typeIconMap: Record<QuestionCardProps["type"], typeInfo> = {
+	SHORT_TEXT: {
+		icon: <CaseSensitive />,
+		label: "文字簡答"
+	},
+	LONG_TEXT: {
+		icon: <TextAlignStart />,
+		label: "文字詳答"
+	},
+	SINGLE_CHOICE: {
+		icon: <List />,
+		label: "單選選擇題"
+	},
+	MULTIPLE_CHOICE: {
+		icon: <SquareCheckBig />,
+		label: "核取方塊"
+	},
+	DROPDOWN: {
+		icon: <Rows3 />,
+		label: "下拉選單"
+	},
+	DETAILED_MULTIPLE_CHOICE: {
+		icon: <LayoutList />,
+		label: "詳細核取方塊"
+	},
+	UPLOAD_FILE: {
+		icon: <CloudUpload />,
+		label: "檔案上傳"
+	},
+	LINEAR_SCALE: {
+		icon: <Ellipsis />,
+		label: "線性刻度"
+	},
+	RANKING: {
+		icon: <ListOrdered />,
+		label: "排序"
+	},
+	DATE: {
+		icon: <Calendar />,
+		label: "日期選擇"
+	}
+};
+
+export const QuestionCard = (props: QuestionCardProps) => {
+	const { type, removeQuestion } = props;
+
+	const [isToggled, setIsToggled] = useState(false);
+
+	const handleToggle = () => {
+		setIsToggled(!isToggled);
+	};
+
+	return (
+		<section className={styles.card} onClick={handleToggle}>
+			{isToggled ? (
+				<div
+					onClick={e => {
+						e.stopPropagation();
+					}}
+					className={styles.content}
+				>
+					<div className={styles.header}>
+						<div className={styles.input}>
+							<Input
+								value={props.title}
+								onChange={e => {
+									props.onTitleChange && props.onTitleChange(e.target.value);
+								}}
+								placeholder="Question 標題"
+								variant="flushed"
+								themeColor="--comment"
+								textSize="h2"
+							/>
+							<Input
+								value={props.description}
+								onChange={e => {
+									props.onDescriptionChange && props.onDescriptionChange(e.target.value);
+								}}
+								placeholder="這裡可以寫一段描述"
+								variant="flushed"
+								themeColor="--comment"
+							/>
+						</div>
+						<div>
+							<Button variant="secondary" className={styles.typeButton}>
+								{typeIconMap[type].icon} {typeIconMap[type].label}
+							</Button>
+						</div>
+					</div>
+					<div className={styles.actions}>
+						<Copy />
+						<Trash2 onClick={removeQuestion} />
+						<div className={`${styles.switch}`}>
+							<p className={`${styles.label}`}>必填</p>
+							<Switch />
+						</div>
+					</div>
+				</div>
+			) : (
+				<div className={styles.preview}>
+					{typeIconMap[type].icon}
+					<p>{props.title || "Question 標題"}</p>
+				</div>
+			)}
+		</section>
+	);
+};
