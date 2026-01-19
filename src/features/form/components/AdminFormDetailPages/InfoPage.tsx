873d075b79c45e07b41e0c68dd0f3952be6d4ea2
Kyle9410-Chen <kyle941010@gmail.com>
2026-01-19 21:05:55 +0800

    feat: add info page ui

---
+++ src/features/form/components/AdminFormDetailPages/InfoPage.tsx
@@ -0,0 +1,44 @@
+import { Input, Switch } from "@/shared/components";
+import styles from "./InfoPage.module.css";
+
+export const AdminFormInfoPage = () => {
+	return (
+		<>
+			<div className={styles.container}>
+				<h3>表單資訊</h3>
+				<section className={styles.seciton}>
+					<div className={`${styles.count}`}>
+						<div className={`${styles.item}`}>
+							<h2 className={`${styles.value}`}>15</h2>
+							<p className={styles.value}>填寫中</p>
+						</div>
+						<div className={`${styles.item}`}>
+							<h2 className={`${styles.value}`}>20</h2>
+							<p className={styles.value}>已提交</p>
+						</div>
+					</div>
+				</section>
+				<h3>表單設定</h3>
+				<Input label="確認訊息" placeholder="輸入表單提交後顯示的訊息" />
+				<div className={`${styles.switch}`}>
+					<p className={`${styles.label}`}>需登入（收集電子郵件）</p>
+					<Switch />
+				</div>
+				<div className={`${styles.switch}`}>
+					<p className={`${styles.label}`}>允許編輯回覆</p>
+					<Switch />
+				</div>
+				<div className={`${styles.switch}`}>
+					<p className={`${styles.label}`}>所有問題設為必填</p>
+					<Switch />
+				</div>
+				<div className={`${styles.switch}`}>
+					<p className={`${styles.label}`}>限定回答時間</p>
+					<Switch />
+				</div>
+				<Input label="開始日期" type="date" defaultValue={new Date().toISOString().split("T")[0]} />
+				<Input label="結束日期" type="date" defaultValue={new Date().toISOString()} />
+			</div>
+		</>
+	);
+};
