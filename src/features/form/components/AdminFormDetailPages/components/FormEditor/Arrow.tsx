120404c38e08efef17cc6919a7bfe540592c7200
Kyle9410-Chen <kyle941010@gmail.com>
2026-02-04 17:30:30 +0800

    feat: add strokeLinecap and strokeLinejoin attributes to Arrow component SVG

--- src/features/form/components/AdminFormDetailPages/components/FormEditor/Arrow.tsx
+++ src/features/form/components/AdminFormDetailPages/components/FormEditor/Arrow.tsx
@@ -9,7 +9,7 @@ interface ArrowProps extends React.SVGAttributes<SVGSVGElement> {
 export const Arrow = ({ type = "info", line = "solid", className, ...props }: ArrowProps) => {
 	return (
 		<div className={`${styles.arrowContainer} ${className || ""}`}>
-			<svg className={`${styles.arrow}`} {...props} transform="rotate(180)">
+			<svg className={`${styles.arrow}`} {...props} transform="rotate(180)" strokeLinecap="round" strokeLinejoin="round">
 				<line x1="50%" y1="0" x2="50%" y2="100%" className={`${styles[type]}`} strokeWidth="2px" strokeDasharray={line === "dashed" ? "4 2" : undefined} />
 				<g transform="translate(28, 0)">
 					<line x1="0" y1="8" x2="4" y2="0" className={`${styles[type]}`} strokeWidth="2px" />
