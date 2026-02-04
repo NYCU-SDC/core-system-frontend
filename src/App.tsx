f3cdead4ee7e5ce2f392cd32b01c27bb30963808
Kyle9410-Chen <kyle941010@gmail.com>
2026-02-04 13:47:08 +0800

    feat: remove ToastProvider from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { ToastProvider } from "@radix-ui/react-toast";
 import { AppRouter } from "./routes/AppRouter";
 import { ToastProvider } from "./shared/components";
 
 const App = () => {
-	return (
-		<ToastProvider>
-			<AppRouter />
-		</ToastProvider>
-	);
+	return <AppRouter />;
 };
 
 export default App;
