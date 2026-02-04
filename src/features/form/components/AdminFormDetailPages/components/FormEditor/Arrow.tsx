245435aeed5048db4ece45df60bac85f01c57218
Kyle9410-Chen <kyle941010@gmail.com>
2026-02-04 17:24:15 +0800

    feat: refactor Arrow component to improve structure and add icon rendering

--- src/features/form/components/AdminFormDetailPages/components/FormEditor/Arrow.tsx
+++ src/features/form/components/AdminFormDetailPages/components/FormEditor/Arrow.tsx
@@ -1,3 +1,4 @@
+import { Check, X } from "lucide-react";
 import styles from "./Arrow.module.css";
 
 interface ArrowProps extends React.SVGAttributes<SVGSVGElement> {
@@ -7,10 +8,16 @@ interface ArrowProps extends React.SVGAttributes<SVGSVGElement> {
 
 export const Arrow = ({ type = "info", line = "solid", className, ...props }: ArrowProps) => {
 	return (
-		<svg className={`${styles.arrow} ${className || ""}`} {...props}>
-			<line x1="50%" y1="0" x2="50%" y2="100%" className={`${styles[type]}`} strokeWidth="2px" strokeDasharray={line === "dashed" ? "4 2" : undefined} />
-			<line x1="calc(50% - 0.3rem)" y1="calc(100% - 0.5rem)" x2="50%" y2="100%" className={`${styles[type]}`} strokeWidth="2px" />
-			<line x1="calc(50% + 0.3rem)" y1="calc(100% - 0.5rem)" x2="50%" y2="100%" className={`${styles[type]}`} strokeWidth="2px" />
-		</svg>
+		<div className={`${styles.arrowContainer} ${className || ""}`}>
+			<svg className={`${styles.arrow}`} {...props} transform="rotate(180)">
+				<line x1="50%" y1="0" x2="50%" y2="100%" className={`${styles[type]}`} strokeWidth="2px" strokeDasharray={line === "dashed" ? "4 2" : undefined} />
+				<g transform="translate(28, 0)">
+					<line x1="0" y1="8" x2="4" y2="0" className={`${styles[type]}`} strokeWidth="2px" />
+					<line x1="4" y1="0" x2="8" y2="8" className={`${styles[type]}`} strokeWidth="2px" />
+				</g>
+			</svg>
+			{type === "fail" && <X x="60%" y="50%" size={12} strokeWidth="4px" className={`${styles[type]} ${styles.icon}`} />}
+			{type === "success" && <Check x="60%" y="50%" size={12} strokeWidth="4px" className={`${styles[type]} ${styles.icon}`} />}
+		</div>
 	);
 };
