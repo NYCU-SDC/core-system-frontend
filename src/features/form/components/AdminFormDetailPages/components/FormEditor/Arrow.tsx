e44ee0c09a7581992f3e5a2a986d9545a0c4695d
Kyle9410-Chen <kyle941010@gmail.com>
2026-02-04 17:32:35 +0800

    feat: simplify Arrow component SVG structure by removing unnecessary group elements

--- src/features/form/components/AdminFormDetailPages/components/FormEditor/Arrow.tsx
+++ src/features/form/components/AdminFormDetailPages/components/FormEditor/Arrow.tsx
@@ -11,10 +11,8 @@ export const Arrow = ({ type = "info", line = "solid", className, ...props }: Ar
 		<div className={`${styles.arrowContainer} ${className || ""}`}>
 			<svg className={`${styles.arrow}`} {...props} transform="rotate(180)" strokeLinecap="round" strokeLinejoin="round">
 				<line x1="50%" y1="0" x2="50%" y2="100%" className={`${styles[type]}`} strokeWidth="2px" strokeDasharray={line === "dashed" ? "4 2" : undefined} />
-				<g transform="translate(28, 0)">
-					<line x1="0" y1="8" x2="4" y2="0" className={`${styles[type]}`} strokeWidth="2px" />
-					<line x1="4" y1="0" x2="8" y2="8" className={`${styles[type]}`} strokeWidth="2px" />
-				</g>
+				<line x1="28" y1="6" x2="32" y2="0" className={`${styles[type]}`} strokeWidth="2px" />
+				<line x1="32" y1="0" x2="36" y2="6" className={`${styles[type]}`} strokeWidth="2px" />
 			</svg>
 			{type === "fail" && <X x="60%" y="50%" size={12} strokeWidth="4px" className={`${styles[type]} ${styles.icon}`} />}
 			{type === "success" && <Check x="60%" y="50%" size={12} strokeWidth="4px" className={`${styles[type]} ${styles.icon}`} />}
