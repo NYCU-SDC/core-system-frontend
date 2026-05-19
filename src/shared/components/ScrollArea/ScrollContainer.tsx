f03794725379a6d194ae23ce85cfbf7001a6a6e8
ching <a0970750975@gmail.com>
2026-05-20 01:14:48 +0800

    fix: prettier ScrollContainer

--- src/shared/components/ScrollArea/ScrollContainer.tsx
+++ src/shared/components/ScrollArea/ScrollContainer.tsx
@@ -1,21 +1,19 @@
-import { forwardRef } from "react";
 import type { ComponentPropsWithoutRef, ReactNode } from "react";
+import { forwardRef } from "react";
 import styles from "./ScrollContainer.module.css";
 
 export interface ScrollContainerProps extends ComponentPropsWithoutRef<"div"> {
 	children: ReactNode;
 }
 
-export const ScrollContainer = forwardRef<HTMLDivElement, ScrollContainerProps>(
-	({ children, className, ...props }, ref) => {
-		const classes = [styles.scrollContainer, className].filter(Boolean).join(" ");
+export const ScrollContainer = forwardRef<HTMLDivElement, ScrollContainerProps>(({ children, className, ...props }, ref) => {
+	const classes = [styles.scrollContainer, className].filter(Boolean).join(" ");
 
-		return (
-			<div ref={ref} className={classes} {...props}>
-				{children}
-			</div>
-		);
-	}
-);
+	return (
+		<div ref={ref} className={classes} {...props}>
+			{children}
+		</div>
+	);
+});
 
 ScrollContainer.displayName = "ScrollContainer";
